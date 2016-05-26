02de323ae09658ad24aa0cca497e738fa338e35f
BogdanKo <bogdan.kondrusik@gmail.com>
2016-05-26 08:16:28 +0300

    Add files via upload

---
+++ cameraTranslate.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraTranslate : MonoBehaviour {
+	public GameObject cameraGO;
+
+	private bool click = false;
+	private Vector2 curPos = Vector2.zero;
+	private Vector3 beginPos;
+
+	public float mouse;
+	public float cameraMinX;
+	public float cameraMaxX;
+	public float cameraMinZ;
+	public float cameraMaxZ;
+
+	public float minScale;
+	public float maxScale;
+	// Use this for initialization
+	void Start () {
+		beginPos = cameraGO.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//click
+		if (Input.GetMouseButtonDown(0)) {
+			click = true;
+			curPos = new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+		} else if (Input.GetMouseButtonUp(0)) {
+			click = false;
+			curPos = Vector2.zero;
+		}
+		if (click) {
+			curPos -= new Vector2(Input.mousePosition.x,Input.mousePosition.y);
+			Vector3 trans = new Vector3(curPos.x/mouse, 0, curPos.y/mouse);
+			Vector3 newPos = cameraGO.transform.position + trans;
+			if (newPos.x>=cameraMinX && newPos.x<=cameraMaxX && newPos.z>=cameraMinZ && newPos.z<=cameraMaxZ) {
+				cameraGO.transform.position += trans;
+			}
+			curPos = Input.mousePosition;
+		}
+		if(Input.mouseScrollDelta.y!=0) {
+			float newScale = cameraGO.transform.eulerAngles.x + Input.mouseScrollDelta.y;
+			if (newScale >= minScale && newScale <= maxScale) {
+				cameraGO.transform.eulerAngles = new Vector3(newScale, 0, 0);
+			}
+		}
+	}
+}
---
+++ generate.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generate : MonoBehaviour {
+	public GameObject tree;
+	public GameObject stone;
+	public float treeY;
+	public float treeRot;
+	public float stoneY;
+	public float terrainWidth;
+	public int radius;
+	public int maxNumber;
+	public int minNumber;
+	
+	// Use this for initialization
+	void Start () {
+		int current = Random.Range (minNumber, maxNumber);
+		for (int i=0; i<current; i++) {
+			int cur = (int)Random.Range(0,2);
+			Vector3 pos = Vector3.zero;
+			//tree
+			if(cur==0) {
+				pos = new Vector3(
+					Random.Range(-terrainWidth,terrainWidth),
+					treeY,
+					Random.Range(-terrainWidth,terrainWidth));
+				//stone
+			} else {
+				pos = new Vector3(
+					Random.Range(-terrainWidth,terrainWidth) + 6.29f,
+					stoneY,
+					Random.Range(-terrainWidth,terrainWidth) + 16.15f);
+			}
+			if((pos.x*pos.x + pos.z*pos.z)>=(radius*radius)){
+				Quaternion rot = Quaternion.Euler(0, treeRot, 0);
+				if(cur==0) {
+					Instantiate(tree,pos,rot);
+				} else {
+					Instantiate(stone,pos,rot);
+				}
+			} else 
+				i--;
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+}
